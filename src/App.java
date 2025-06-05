bd03d25a88916f7388cd1cc9601f7c190cabbab1
Xristianz <moscosocristhian130@gmail.com>
2025-06-05 07:27:20 -0500

    Deber

--- src/App.java
+++ src/App.java
@@ -1,47 +1,77 @@
+
+
 import controllers.Cola;
 import controllers.ColaG;
+import controllers.Stack;
+import controllers.StackG;
 import models.Persona;
 
 public class App {
-     public static void main(String[] args) throws Exception {
-       controllers.Stack stack = new controllers.Stack();
+    public static void main(String[] args) throws Exception {
+        Stack stack = new Stack();
         stack.push(10);
         stack.push(-1);
-        stack.push(0);
+        stack.push (0);
         stack.push(5);
         stack.printStack();
         stack.pop();
         stack.peek();
         stack.printStack();
 
+        //Stack Generica
+        System.out.println("\nStack Generica");
 
+        StackG<Integer> stackG = new StackG<Integer>();
+        stackG.push(1);
+        stackG.push(4);
+        stackG.printStack();
 
+        StackG<String> stackG1 = new StackG<String>();
+        stackG1.push("Uno");
+        stackG1.push("Cuatro");
+        stackG1.printStack();
+
+        //FIFO  - PRIMERO EN ENTRAR, PRIMERO EN SALIR
+        //Colas
+        System.out.println("\nColas");
+
+        // Cola no generica
         Cola cola = new Cola();
         cola.add(5);
         cola.add(7);
         cola.add(10);
-        System.out.println("tam = " + cola.size);
+        System.out.println("Tam = " + cola.size);
+        cola.printCola();
+        cola.remove();
+        System.out.println("Tam = " + cola.size);
+        cola.printCola();
 
-        Persona p1= new Persona("Pablo");
-        Persona p2= new Persona("Juan");
-        Persona p3= new Persona("Maria");
+        System.out.println("Peek: " + stack.peek());
+        // Cola generica
+
+        Persona p1 = new Persona("Pablo");
+        Persona p2 = new Persona("Juan");
+        Persona p3 = new Persona("Maria");
+
+        System.out.println("\nCola Generica");
 
         ColaG<Persona> colaPersonas = new ColaG<Persona>();
         colaPersonas.add(p1);
         colaPersonas.add(p2);
         colaPersonas.add(p3);
-        System.out.println(colaPersonas.size());
+        System.out.println("Tam = " + colaPersonas.size());
         colaPersonas.printCola();
-        System.out.println("Atiende a la persona => " + colaPersonas.remove());
-        System.out.println(colaPersonas.size());
+        System.out.println("Atiende a la persona: " + colaPersonas.remove().getNombre());
+        System.out.println("Tam = " + colaPersonas.size());
         colaPersonas.printCola();
         
 
+        System.out.println("Maria esta en la posicion "+colaPersonas.findByName("Maria"));
+        System.out.println("Elimina");
+        colaPersonas.removeByName("David");
+        colaPersonas.printCola();
 
 
 
-
-        
     }
-
 }
--- src/controllers/ColaG.java
+++ src/controllers/ColaG.java
@@ -4,6 +4,7 @@ import java.util.EmptyStackException;
 import models.NodeGeneric;
 
 public class ColaG<T> {
+
     private NodeGeneric<T> primero;
     private NodeGeneric<T> ultimo;
     private int size;
@@ -14,14 +15,18 @@ public class ColaG<T> {
         this.size = 0;
     }
 
-    public void add(T value) {
-        NodeGeneric<T> newNode = new NodeGeneric<>(value);
+    public boolean isEmpty() {
+        return primero == null;
+    }
+
+    public void add(T valor) {
+        NodeGeneric<T> nuevo = new NodeGeneric<T>(valor);
         if (isEmpty()) {
-            primero = newNode;
-            ultimo = newNode;
+            primero = nuevo;
+            ultimo = nuevo;
         } else {
-            ultimo.setNext(newNode);
-            ultimo = newNode;
+            ultimo.setNext(nuevo);
+            ultimo = nuevo;
         }
         size++;
     }
@@ -29,34 +34,76 @@ public class ColaG<T> {
     public T remove() {
         if (isEmpty()) {
             throw new EmptyStackException();
-        } else {
-            T value = primero.getValue();
-            primero = primero.getNext(); 
-            size--;
-            return value;
         }
+        T valor = primero.getValue();
+        primero = primero.getNext();
+        if (primero == null) {
+            ultimo = null;
+        }
+        size--;
+        return valor;
     }
 
     public T peek() {
-        if (isEmpty()) {
+        if (isEmpty())
             throw new EmptyStackException();
-        }
         return primero.getValue();
     }
 
-    public boolean isEmpty() {
-        return primero == null;
+    public int size() {
+        return size;
     }
 
     public void printCola() {
-    NodeGeneric<T> actual = primero;
-    while (actual != null) {
-        System.out.println(actual.getValue()); 
-        actual = actual.getNext();
-    }
+        if (isEmpty()) {
+            System.out.println("La cola está vacía.");
+            return;
+        }
+        NodeGeneric<T> current = primero;
+        while (current != null) {
+            System.out.print(current.getValue() + " ");
+            current = current.getNext();
+        }
 
+        System.out.println();
+
+        
     }
-    public int size() {
-        return size;
+    public int findByName(String name) {
+        int posicion = 0;
+        NodeGeneric<T> node = primero;
+        while (node != null) {
+            if (node.getValue().toString().equals(name)) {
+                return posicion;
+            }
+            node = node.getNext();
+            posicion++;
+        }
+        return -1;
+    }
+    public void removeByName(String name) {
+        if (isEmpty())
+            throw new EmptyStackException();
+        if (primero.getValue().toString().equals(name)) {
+            primero = primero.getNext();
+            if (primero == null) {
+                ultimo = null;
+            }
+            size--;
+            return;
+        }
+        NodeGeneric<T> actual = primero;
+        NodeGeneric<T> anterior = null;
+        while (actual != null && !actual.getValue().toString().equals(name)) {
+            anterior = actual;
+            actual = actual.getNext();
+        }
+        if (actual != null) {
+            anterior.setNext(actual.getNext());
+            if (actual == ultimo) {
+                ultimo = anterior;
+            }
+            size--;
+        }    
     }
 }
